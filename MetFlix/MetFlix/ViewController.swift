5cd0ca07df6400c8ec1d78677a09cbaa9596ff19
vamsi <vamsi11889@gmail.com>
2024-07-12 10:38:10 +0530

    Initial commit

---
+++ MetFlix/MetFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MetFlix
+//
+//  Created by apple on 12/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
